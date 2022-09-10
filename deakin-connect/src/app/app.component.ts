3adb5640c23e1352ffe019622fb27e7f0b7bc623
roz-mj <113166342+JP-Reeves@users.noreply.github.com>
2022-09-10 19:13:50 +1000

    Added pages, router guard, and navbars
    Added pages:
    +Login
    +Explore
    +Events
    +Connect
    +Settings
    
    Added router guard, requiring the user to be logged in to access all pages except login page
    
    Added navigation bars for mobile and desktop
    
    No animations added yet
    Login button currently sets user as logged in with no authentication at all

--- deakin-connect/src/app/app.component.ts
+++ deakin-connect/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AuthenticationService } from './authentication.service';
+import { Router, RouterEvent } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,31 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'deakin-connect';
+  constructor(public authServ: AuthenticationService, private router: Router) {}
+  public isLoggedIn(){ return this.authServ.loggedIn() }
+
+  // name : icon (google icons)
+  // For routing to work the path(app-routing.module.ts) must be the same as the name below
+  public navItems = {
+    'Explore' : 'explore',
+    'Events' : 'event',
+    'Connect' : 'group',
+    'Settings' : 'settings'
+  }
+
+  // index of current navItem
+  public currentNavItemIndex = 0;
+
+  // Required to prevent the *ngFor loop from alphabetically sorting the navItems dict...
+  public keypipe() { return 0; }
+
+  // Used to track if sideNav is open
+  public sideNavOpen = false;
+
+  // Called when a nav item is clicked, changes current page & closes side nav
+  public updatePage(index: number, pageName: string){
+    this.sideNavOpen = false;
+    this.currentNavItemIndex = index;
+    this.router.navigate([pageName]);
+  }
 }
